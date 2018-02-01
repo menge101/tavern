5545e6c4a736ae064c2c647c7a96b11fc1de31d5
Nathan Menge <nathan.menge@arcadiasolutions.com>
2018-01-31 21:47:51 -0500

    [Refactor] Removed unneeded implementation of a dictionary comparison for the built in version.

--- app/models/persistence/base.py
+++ app/models/persistence/base.py
@@ -24,32 +24,6 @@ class BaseModel(Model):
                                       conditional_operator=conditional_operator, **expected_values)
 
     def __eq__(self, other):
-        return self.eq_dict(self.__dict__, other.__dict__)
-
-    @classmethod
-    def eq_dict(cls, a, b):
-        if len(a) != len(b):
+        if not isinstance(other, self.__class__):
             return False
-        a_keys = list(a.keys())
-        b_keys = list(b.keys())
-        a_keys.sort()
-        b_keys.sort()
-        if a_keys != b_keys:
-            return False
-        for key in a_keys:
-            a_value = a[key]
-            b_value = b[key]
-            if hasattr(a_value, 'keys'):
-                result = cls.eq_dict(a_value, b_value)
-            elif hasattr(a_value, 'sort'):
-                try:
-                    a_value.sort()
-                    b_value.sort()
-                except TypeError:
-                    pass
-                result = a_value == b_value
-            else:
-                result = a_value == b_value
-            if result is False:
-                return False
-        return True
+        return self.__dict__ == other.__dict__
