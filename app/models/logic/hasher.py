9b612319e09e3f9cdce8fb99176a1d5a165a1681
Nathan Menge <nathan.menge@arcadiasolutions.com>
2018-02-12 21:43:26 -0500

    [Hasher Logic] Updated hasher logic models to properly create and use data models.

--- app/models/logic/hasher.py
+++ app/models/logic/hasher.py
@@ -4,15 +4,22 @@ from app.models.persistence.hasher import HasherDataModel
 
 
 class HasherLogicModel(LogicBase):
-    def __init__(self, hash_name, mother_kennel, hasher_id=None, contact_info=None, real_name=None, user=None,
-                 lower_hash_name=None, persistence_object=None):
+    def __init__(self, hash_name, mother_kennel, mother_kennel_name_lower=None, hasher_id=None, contact_info=None,
+                 real_name=None, user=None, lower_hash_name=None, persistence_object=None):
         if lower_hash_name is None:
             lower_hash_name = hash_name.lower()
         super().__init__()
         self.hasher_id = ulid() if hasher_id is None else hasher_id
         self.hash_name = hash_name
         self.lower_hash_name = lower_hash_name
-        self.mother_kennel = mother_kennel
+        if hasattr(mother_kennel, 'create'):
+            self.mother_kennel = self.map_mother_kennel(mother_kennel)
+        else:
+            self.mother_kennel = mother_kennel
+        if mother_kennel_name_lower is None:
+            self.mother_kennel_name_lower = self.mother_kennel['name'].lower()
+        else:
+            self.mother_kennel_name_lower = mother_kennel_name_lower
         self.contact_info = contact_info
         self.real_name = real_name
         self.user = user
@@ -37,3 +44,7 @@ class HasherLogicModel(LogicBase):
         attribute_dict['persistence_object'] = result
         attribute_dict['hasher_id'] = result.hasher_id
         return HasherLogicModel(**attribute_dict)
+
+    @staticmethod
+    def map_mother_kennel(momma):
+        return {'kennel_id': momma.kennel_id, 'name': momma.name, 'acronym': momma.acronym}
--- tests/models/logic/hasher.py
+++ tests/models/logic/hasher.py
@@ -1,5 +1,7 @@
 import unittest
-from app.models.logic.hasher import AlreadyExists, HasherLogicModel
+from app.models.logic.hasher import HasherLogicModel
+from app.models.logic.kennel import KennelLogicModel
+from app.models.persistence import AlreadyExists
 from app.models.persistence.hasher import HasherDataModel
 
 
@@ -7,7 +9,7 @@ class HasherLogicTests(unittest.TestCase):
     def setUp(self):
         self.hasher_id = 'test_id'
         self.hash_name = 'Testy Cream'
-        self.mother_kennel = 'test_kennel_id'
+        self.mother_kennel = KennelLogicModel('Test Kennel 1', 'TK1H3')
         HasherDataModel.Meta.host = 'http://localhost:8000'
         if HasherDataModel.exists():
             HasherDataModel.delete_table()
@@ -15,11 +17,14 @@ class HasherLogicTests(unittest.TestCase):
 
     def test_init_from_lookup(self):
         HasherDataModel(self.hasher_id, hash_name=self.hash_name, lower_hash_name=self.hash_name.lower(),
-                        mother_kennel=self.mother_kennel).save()
-        x = HasherLogicModel.lookup_by_id(self.hasher_id)
-        self.assertEqual(self.hasher_id, x.hasher_id)
-        self.assertEqual(self.hash_name, x.hash_name)
-        self.assertEqual(self.mother_kennel, x.mother_kennel)
+                        mother_kennel=HasherLogicModel.map_mother_kennel(self.mother_kennel),
+                        mother_kennel_name_lower=self.mother_kennel.name.lower()).save()
+        actual = HasherLogicModel.lookup_by_id(self.hasher_id)
+        self.assertEqual(self.hasher_id, actual.hasher_id)
+        self.assertEqual(self.hash_name, actual.hash_name)
+        self.assertEqual(self.mother_kennel.kennel_id, actual.mother_kennel.kennel_id)
+        self.assertEqual(self.mother_kennel.name, actual.mother_kennel.name)
+        self.assertEqual(self.mother_kennel.acronym, actual.mother_kennel.acronym)
 
     def test_lookup_hasher_doesnt_exist(self):
         with self.assertRaises(HasherDataModel.DoesNotExist):
@@ -30,7 +35,9 @@ class HasherLogicTests(unittest.TestCase):
         expected = HasherLogicModel.lookup_by_id(actual.hasher_id)
         self.assertEqual(actual, expected)
         self.assertEqual(actual.hash_name, self.hash_name)
-        self.assertEqual(actual.mother_kennel, self.mother_kennel)
+        self.assertEqual(self.mother_kennel.kennel_id, actual.mother_kennel.kennel_id)
+        self.assertEqual(self.mother_kennel.name, actual.mother_kennel.name)
+        self.assertEqual(self.mother_kennel.acronym, actual.mother_kennel.acronym)
 
     def test_redundant_create(self):
         HasherLogicModel.create(self.hash_name, self.mother_kennel)
